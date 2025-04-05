5d87c8c2259056168ee0e3b19ca594e7fdaa866b
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-04-05 17:09:10 -0300

    feat: add Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header/Header.componet";
 
 function App() {
     return (
         <>
-            
+            <Header/>
         </>
-    );
+    )
 }
 
 export default App;
