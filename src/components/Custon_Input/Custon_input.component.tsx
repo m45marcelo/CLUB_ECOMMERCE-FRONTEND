2f06362e57836ba255071d165e6d721d35f7303e
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-04-10 19:35:25 -0300

    feat: add React Hook Form validation to Login Page and Custom Input

--- src/components/Custon_Input/Custon_input.component.tsx
+++ src/components/Custon_Input/Custon_input.component.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, InputHTMLAttributes } from "react";
+import React, { FunctionComponent, InputHTMLAttributes } from "react";
 
 //Styles
 import { CustomInputContainer } from "./Custom_Input.styles";
@@ -7,8 +7,12 @@ interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement>{
     hasError?: boolean
 }
 
-const CustonInput: FunctionComponent<CustomInputProps> = ({hasError, ...rest}) => {
-    return <CustomInputContainer hasError={hasError} {...rest} />
-}
+const CustonInput: FunctionComponent<CustomInputProps> = React.forwardRef(
+    (props, ref) => {
+        return <CustomInputContainer {...props} ref={ref as any} />
+    }
+)
+
+CustonInput.displayName = 'CustomInput'
 
 export default CustonInput
--- src/pages/Login/login.page.tsx
+++ src/pages/Login/login.page.tsx
@@ -1,39 +1,66 @@
 import { BsGoogle } from "react-icons/bs";
 import { FiLogIn } from "react-icons/fi";
-
+import { useForm } from "react-hook-form";
 //Components
-import CustomButton from "../../components/Custom_Button/Custom_Button.component"
-import Header from "../../components/Header/Header.componet"
+import CustomButton from "../../components/Custom_Button/Custom_Button.component";
+import Header from "../../components/Header/Header.componet";
 
 //styles
-import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, LoginSubtitle } from "./login.styles"
+import {
+    LoginContainer,
+    LoginContent,
+    LoginHeadline,
+    LoginInputContainer,
+    LoginSubtitle,
+} from "./login.styles";
 import CustonInput from "../../components/Custon_Input/Custon_input.component";
 
 const LoginPage = () => {
-    return(
+    const { register, handleSubmit, formState: {errors}} = useForm();
+
+    const handleSubmitPress = (data: any) => {
+        console.log({ data })
+    }
+
+    return (
         <>
-        <Header/>
-        <LoginContainer>
-            <LoginContent>
-            <LoginHeadline>Entre com a sua conta</LoginHeadline>
-            <CustomButton startIcon={<BsGoogle size={18}/>}>Entrar com o Google</CustomButton>
+            <Header />
+            <LoginContainer>
+                <LoginContent>
+                    <LoginHeadline>Entre com a sua conta</LoginHeadline>
+                    <CustomButton startIcon={<BsGoogle size={18} />}>
+                        Entrar com o Google
+                    </CustomButton>
 
-            <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
+                    <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
 
-            <LoginInputContainer>
-                <p>E-mail</p>
-                <CustonInput placeholder="Digite seu e-mail" />
-            </LoginInputContainer>
-            <LoginInputContainer>
-                <p>Senha</p>
-                <CustonInput placeholder="Digete sua senha" />
-            </LoginInputContainer>
+                    <LoginInputContainer>
+                        <p>E-mail</p>
+                        <CustonInput
+                            hasError={!!errors?.email}
+                            placeholder="Digite seu e-mail"
+                            {...register("email", { required: true })}
+                        />
+                    </LoginInputContainer>
+                    <LoginInputContainer>
+                        <p>Senha</p>
+                        <CustonInput
+                            hasError={!!errors?.senha}
+                            placeholder="Digete sua senha"
+                            {...register("senha", { required: true })}
+                        />
+                    </LoginInputContainer>
 
-            <CustomButton startIcon={<FiLogIn size={18}/>}>Entrar</CustomButton>
-            </LoginContent>
-        </LoginContainer>
-    </>
-    )
-}
+                    <CustomButton
+                        startIcon={<FiLogIn size={18} />}
+                        onClick={handleSubmit(handleSubmitPress)}>
+                        Entrar
+                    </CustomButton>
 
-export default LoginPage
+                </LoginContent>
+            </LoginContainer>
+        </>
+    );
+};
+
+export default LoginPage;
